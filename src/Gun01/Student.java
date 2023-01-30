600c6065c2d5d1528ecce342e8d7a1bffb29f422
YusufuMMMMM <ilker_kilic2541@outlook.com>
2023-01-30 19:24:00 +0300

    Initial commit

---
+++ src/Gun01/Student.java
@@ -0,0 +1,11 @@
+package Gun01;
+
+public class Student {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+
+        System.out.println("Task 2");
+        System.out.println("Task 3");
+    }
+}
